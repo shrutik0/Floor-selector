0413c62e1db49d66b25194a4b57e920379a3c9be
Chaitanya360 <chaitanyajadhav360@gmail.com>
2022-02-07 11:56:48 +0530

    changed base url to public

--- src/data/index.js
+++ src/data/index.js
@@ -12,8 +12,8 @@ export const FLOORS_VIEWPORTS = {
   D: "0 0 941 991",
 };
 
-export const baseUrl = "http://127.0.0.1:5000";
-// export const baseUrl = "https://bel-air.herokuapp.com";
+// export const baseUrl = "http://127.0.0.1:5000";
+export const baseUrl = "https://bel-air.herokuapp.com";
 
 export const TOWER_NAMES_LIST = ["A", "B", "C", "D"];
 export const FLOORS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
