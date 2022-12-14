435373ecfd4af9f43524d70b7b32c7d5a9f93c0a
shrutik0 <shrutik4shangupta27@gmail.com>
2022-12-14 14:50:46 +0530

    nms

--- src/data/index.js
+++ src/data/index.js
@@ -4,7 +4,7 @@
 // export const baseUrl = "https://bel-air.herokuapp.com";
 export const baseUrl = "https://anshulpadyal.pythonanywhere.com";
 
-export const TOWER_NAMES_LIST = ["A", "B", "C", "D","E","F","G","H"];
+export const TOWER_NAMES_LIST = ["A", "B", "C", "D"];
 export const FLOORS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 
 export const FLOORS_COMPASS_ANGLES = {
